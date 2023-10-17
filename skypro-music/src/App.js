b8581e787d3e59aa3485df128c1e4e605d7491ca
Burov Dmitry <burovdo88@gmail.com>
2023-10-17 21:21:26 +0500

    Начало проекта

---
+++ skypro-music/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Здесь скоро будет Skypro.Music
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//Покрасьте добавленную строку в красный цвет. Для этого отредактируйте стили в файле App.css, остальные стили нужно удалить.
